4fd68cd654831cdc2ada8d6307b24bfadd5fa2ad
Fran Camacho <francisco.camacho.gomez@hotmail.com>
2025-05-02 18:00:45 +0200

    primer commit

---
+++ practica-camacho.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+* @author Fran Camacho
+* @internal si
+* @version    SVN: $Id$
+* Calcular la suma de 2 numeros
+*
+*Esta fucion toma los dos argumentos de entrada $a y $b, los suma y retorna la suma de ambos numeros
+*
+* @param int $a
+* @param int $b
+*
+* @return in la suma de 2numeros
+*/
+function sumar($a, $b)
+{
+        return $a + $b;
+}
